66337c41dd0da602f04ed7782e77333ca7e8fae5
Robby Sahm <robertsahm@gmail.com>
2016-04-25 18:22:34 -0400

    auth features updated

---
+++ src/components/feature.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import * as actions from '../actions'
+import { connect } from 'react-redux'
+
+class Feature extends Component {
+  componentWillMount() {
+    this.props.fetchMessage()
+  }
+
+  render() {
+    return (
+      <div>{this.props.message}</div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { message: state.auth.message }
+}
+
+export default connect(mapStateToProps, actions)(Feature)
